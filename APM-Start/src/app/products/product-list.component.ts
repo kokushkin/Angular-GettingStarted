fccff6f357d6471a7eef66ad0faf6c56f7a286b2
kokushkin <fipsiksi@yandex.ru>
2019-01-18 22:35:02 +0200

    * Data Binding & Pipes: Handling Input with Two-way binding

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
     pageTitle: string = 'Product List!';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
     products: any[] = [
         {
             "productId": 2,
@@ -27,5 +30,9 @@ export class ProductListComponent {
             "starRating": 4.8,
             "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
           }
-    ]
+    ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
